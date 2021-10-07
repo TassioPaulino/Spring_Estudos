fe6b2c952debd3e51648e5601ce68346be68cf5a
Tássio Paulino <tassio_paulino@hotmail.com>
2021-10-07 16:50:10 -0300

    Criado um novo arquivo de configuração XML para demonstar os padrões 'Singleton' e 'Prototype'

---
+++ ProjetoSpringUm/src/com/tassio_paulino/IoC/UsoDemoSingletonPrototype.java
@@ -0,0 +1,22 @@
+package com.tassio_paulino.IoC;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UsoDemoSingletonPrototype {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext2.xml");
+		
+		SecretarioEmpregado maria = contexto.getBean("meuSecretario", SecretarioEmpregado.class);
+		SecretarioEmpregado pedro = contexto.getBean("meuSecretario", SecretarioEmpregado.class);
+		
+		System.out.println(maria);
+		System.out.println(pedro);
+		
+		if(maria == pedro) System.out.println("Apontam ao mesmo objeto.");
+		else System.out.println("Não se tratam do mesmo objeto.");
+		
+		contexto.close();
+	}
+
+}
