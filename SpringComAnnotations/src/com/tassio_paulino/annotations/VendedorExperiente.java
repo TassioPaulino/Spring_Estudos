3a18e0fc5156ca9d1500b21df4d7ef8a8e682cac
Tássio Paulino <tassio_paulino@hotmail.com>
2021-12-17 11:12:08 -0300

    Implementação do estudos sobre as annotations @PostConstruct e @Predestroy

--- SpringComAnnotations/src/com/tassio_paulino/annotations/VendedorExperiente.java
+++ SpringComAnnotations/src/com/tassio_paulino/annotations/VendedorExperiente.java
@@ -1,12 +1,14 @@
 package com.tassio_paulino.annotations;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class VendedorExperiente implements Empregados {
 
 	@Autowired
@@ -22,6 +24,15 @@ public class VendedorExperiente implements Empregados {
 	 * @Autowired public void setNovoRelatorio(ReportavelFinanceiro novoRelatorio) {
 	 * this.novoRelatorio = novoRelatorio; }
 	 */
+	@PostConstruct
+	public void executaDepoisDaCriacao() {
+		System.out.println("Executado depois da criação do Bean.");
+	}
+	
+	@PreDestroy
+	public void executaAntesDaDestruicao() {
+		System.out.println("Executado antes da destruição do Bean.");
+	}
 	
 	@Override
 	public String getTarefas() {
